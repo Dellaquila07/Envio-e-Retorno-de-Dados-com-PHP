8eb9862c78f4517cda0ff4ced84a48f61d2d92da
Kauã Guilherme Rodrigues Dellaquila <91237325+Dellaquila07@users.noreply.github.com>
2021-10-30 22:35:05 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,10 @@
+<?php
+    //Variáveis de Parâmetros
+    $servidor = "localhost";
+    $usuario = "root";
+    $senha = "";
+    $dbname = "project_1";
+
+    // Criando a conexão com o banco de dados
+    $conn= mysqli_connect ($servidor,$usuario,$senha,$dbname);
+?>
